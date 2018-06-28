d7b40ebb5fb9f22483866ddfab60ed9d3813e674
David John Gagne <djgagne@ou.edu>
2018-06-28 14:30:54 -0600

    Added setup.py file and notebooks directory

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(name="mlmicrophysics",
+          version="0.1",
+          description="Machine learning emulator testbed for microphysics."
+          author="David John Gagne and Negin Sobhani",
+          author_email="dgagne@ucar.edu",
+          license="MIT",
+          url="https://github.com/NCAR/mlmicrophysics",
+          packages=["mlmicrophysics"],
+          install_requires=["numpy",
+                            "scipy",
+                            "pandas",
+                            "matplotlib",
+                            "xarray",
+                            "tensorflow",
+                            "keras",
+                            "netcdf4",
+                            "scikit-learn",
+                            "pyyaml"])
