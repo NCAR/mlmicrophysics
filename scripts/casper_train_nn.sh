4185077ece5dabb27f164b03873d86bca6779baa
djgagne@ou.edu <djgagne@ou.edu>
2019-05-24 14:27:17 -0600

    Updated training config

--- scripts/casper_train_nn.sh
+++ scripts/casper_train_nn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l 
-#SBATCH --job-name=nn_run2
+#SBATCH --job-name=nn_run5
 #SBATCH --account=NAML0001
 #SBATCH --time=03:00:00
 #SBATCH --ntasks=8
@@ -8,9 +8,9 @@
 #SBATCH --gres=gpu:v100:1
 #SBATCH --output=train_nn.%j.out
 #SBATCH --mem=250G
-module load gnu/7.3.0 openmpi-x/3.1.0 python/3.6.4 cuda/9.2 
-source /glade/work/dgagne/ncar_pylib_dl/bin/activate
+module load gnu/7.3.0 openmpi-x/3.1.0 python/3.6.8 cuda/10.0 
+source /glade/work/dgagne/ncar_pylib_dl_10/bin/activate
 cd ~/mlmicrophysics/
 python setup.py install
 cd ~/mlmicrophysics/scripts
-python -u train_mp_neural_nets.py ../config/cesm_tau_run_2_train_nn.yml
+python -u train_mp_neural_nets.py ../config/cesm_tau_run5_train_nn.yml
