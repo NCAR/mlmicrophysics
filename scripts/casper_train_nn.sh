e3e2156a03f7c03be8b6cce736acd128651694f1
djgagne@ou.edu <djgagne@ou.edu>
2019-08-01 14:20:53 -0600

    Updated stuff

--- scripts/casper_train_nn.sh
+++ scripts/casper_train_nn.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -l 
 #SBATCH --job-name=nn_run5
 #SBATCH --account=NAML0001
-#SBATCH --time=03:00:00
-#SBATCH --ntasks=8
-#SBATCH --ntasks-per-node=8
+#SBATCH --time=12:00:00
+#SBATCH --ntasks=16
+#SBATCH --ntasks-per-node=16
 #SBATCH --partition=dav
 #SBATCH --gres=gpu:v100:1
 #SBATCH --output=train_nn.%j.out
-#SBATCH --mem=250G
+#SBATCH --mem=400G
 module load gnu/7.3.0 openmpi python/3.6.8 cuda/10.0
 source /glade/work/dgagne/ncar_pylib_dl_10/bin/activate
 cd ~/mlmicrophysics/
